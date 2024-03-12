be2782e62cc2b8913a808ca80738f2b294307d24
growbori <grow.boribori@gmail.com>
2024-03-12 15:53:04 +0900

    0312

---
+++ Web 실습/DAY 5 Django Intro/Django/articles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+# 메인 페이지를 만드는 index 라는 이름의 함수를 작성
+
+def index(request):
+    #  render(요청 객체, 템플릿 경로)
+    return render(request, 'articles/index.html')
